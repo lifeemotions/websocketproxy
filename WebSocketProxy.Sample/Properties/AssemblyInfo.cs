0298e3943e864dfe28e45cfb1ca471720120f400
Tiagooliveiramarques <tiago.d.oliveira.marques@tecnico.ulisboa.pt>
2015-09-03 18:20:51 +0100

    Assembly Info update

--- WebSocketProxy.Sample/Properties/AssemblyInfo.cs
+++ WebSocketProxy.Sample/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LESC.TcpProxy.Sample")]
+[assembly: AssemblyTitle("WebSocketProxy.Sample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LESC.TcpProxy.Sample")]
+[assembly: AssemblyProduct("WebSocketProxy.Sample")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
